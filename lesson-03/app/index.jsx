65780a85cbaf4970da82aca32f196f6ad7369b1d
Jeff Smith <jes@microsoft.com>
2018-03-28 13:46:37 -0700

    Removed old Nav example and replced with creating a layout using a Grid

--- lesson-03/app/index.jsx
+++ lesson-03/app/index.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Imports components from Fluent Web
-import { Header } from '@ms-fw/fw-react/components';
+import { Heading, Paragraph, Image, CallToAction } from '@ms-fw/fw-react/components';
+import { Grid, Column, Page } from '@ms-fw/fw-react/layouts';
 
 // Imports styles from Fluent Web
 const styles = require('../build/public/assets/styles/css/fw-west-european-default-orange.css');
@@ -11,111 +12,7 @@ const styles = require('../build/public/assets/styles/css/fw-west-european-defau
 // ==================================================================
 // Configuring Header component
 import logoMsGray from './assets/images/logos/microsoft-gray.png';
-import meImage from './assets/images/satya.png';
  
-var globalNavigation = {
-    "items": [
-        {
-            "text": "Windows",
-            "href": "#"
-        },
-        {
-            "text": "Surface",
-            "href": "#"
-        },
-        {
-            "text": "Office",
-            "href": "#"
-        },
-        {
-            "text": "Xbox",
-            "href": "#"
-        },
-        {
-            "text": "Azure",
-            "href": "#"
-        },
-        {
-            "buttonText": "More",
-            "id": "moreButton",
-            "items": [
-              {
-                "text": "Business",
-                "href": "https://www.microsoft.com/en-us/store/b/business?icid=TopNavBusinessStore"
-              },
-              {
-                "text": "Students & educators",
-                "href": "https://www.microsoft.com/en-us/store/b/student?icid=TopNavEduStore"
-              },
-              {
-                "text": "Developer",
-                "href": "https://www.microsoft.com/en-us/store/b/developer?icid=CNavDeveloper"
-              },
-              {
-                "text": "Gift cards",
-                "href": "https://www.microsoft.com/en-us/store/b/gift-cards"
-              }
-            ],
-            "expanded": false
-          }
-    ],
-    "logo": {
-        "href": "#",
-        "image": {
-            "src": logoMsGray,
-            "alt": "Microsoft"
-        },
-        "text": "Microsoft"
-    }
-};
-
-var mobileToggle = {
-    "glyph": "global-nav-button",
-    "glyphOnly": true,
-    "text": "Menu"
-};
-
-var mobilePrevious = {
-    "glyph": "chevron-left",
-    "glyphOnly": true,
-    "text": "Previous"
-};
-
-var mobileNext = {
-    "glyph": "chevron-right",
-    "glyphOnly": true,
-    "text": "Next"
-};
-
-var me = {
-    "media": {
-      "src": meImage,
-      "alt": "Microsoft profile picture"
-    },
-    "name": {
-      "text": "Satya"
-    }
-  };
-
-var actions = [
-    {
-        "glyph": "waffle",
-        "glyphOnly": true,
-        "text": "Menu"
-    },
-    {
-        "glyph": "search",
-        "glyphOnly": true,
-        "text": "Search"
-    },
-    {
-        "glyph": "shop-brand",
-        "glyphOnly": true,
-        "href": "#",
-        "text": "Bag"
-    }
-];
-
 // ==================================================================
 // Configuring other components
 
@@ -123,16 +20,269 @@ var actions = [
 // Render app
 
 ReactDOM.render(
-    <div>
-        <Header
-            globalNavigation={ globalNavigation }
-            mobileToggle={ mobileToggle }
-            mobilePrevious={ mobilePrevious }
-            mobileNext={ mobileNext }
-            actions={ actions } 
-            me={ me }
-        />
-    </div>
+    <Page>
+        <Grid 
+            tag='section1'
+            columnCount={12}
+            gutter={6}
+            verticalAlign={'center'}
+            horizontalAlign={'center'}
+            >
+            {/* Row 1 */}
+            <Column
+                row={1}
+                span={[12,12,6,6]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                    level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[2,2,1,1]}
+                span={[12,12,6,6]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                   level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[3,3,2,2]}
+                span={[12,12,4,4]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                   level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[4,4,2,2]}
+                span={[12,12,4,4]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                   level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[5,5,2,2]}
+                span={[12,12,4,4]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                   level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[6,6,3,3]}
+                span={[12,12,6,6]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                   level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[7,7,3,3]}
+                span={[12,12,6,6]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                   level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[8,8,4,4]}
+                span={[12,12,6,6]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                    level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[9,9,4,4]}
+                span={[12,12,6,6]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                    level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+        </Grid>       
+    </Page>
     ,
     document.getElementById('root')
 );
