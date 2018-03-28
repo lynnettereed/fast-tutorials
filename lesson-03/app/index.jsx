decb13eb8e467ffc0bfa6322828f04b3f53eb91a
eljefe223 <37851214+eljefe223@users.noreply.github.com>
2018-03-28 15:26:33 -0700

    update(tutorial): removes navigation and replaces with grid layout in lesson 3 (#16)
    * Added Sub Nav Button so Previous and Next Buttons are relevant
    
    * Added Me component and Image
    
    * Added Me component and Image
    
    * Removed old Nav example and replced with creating a layout using a Grid
    
    * Clean up Markup
    
    * Addressed NITs

--- lesson-03/app/index.jsx
+++ lesson-03/app/index.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Imports components from Fluent Web
-import { Header } from '@ms-fw/fw-react/components';
+import { Heading, Paragraph, Image, CallToAction } from '@ms-fw/fw-react/components';
+import { Grid, Column, Page } from '@ms-fw/fw-react/layouts';
 
 // Imports styles from Fluent Web
 const styles = require('../build/public/assets/styles/css/fw-west-european-default-orange.css');
@@ -11,93 +12,162 @@ const styles = require('../build/public/assets/styles/css/fw-west-european-defau
 // ==================================================================
 // Configuring Header component
 import logoMsGray from './assets/images/logos/microsoft-gray.png';
- 
-var globalNavigation = {
-    "items": [
-        {
-            "text": "Windows",
-            "href": "#"
-        },
-        {
-            "text": "Surface",
-            "href": "#"
-        },
-        {
-            "text": "Office",
-            "href": "#"
-        },
-        {
-            "text": "Xbox",
-            "href": "#"
-        },
-        {
-            "text": "Azure",
-            "href": "#"
-        }
-    ],
-    "logo": {
-        "href": "#",
-        "image": {
-            "src": logoMsGray,
-            "alt": "Microsoft"
-        },
-        "text": "Microsoft"
-    }
-};
-
-var mobileToggle = {
-    "glyph": "global-nav-button",
-    "glyphOnly": true,
-    "text": "Menu"
-};
-
-var mobilePrevious = {
-    "glyph": "chevron-left",
-    "glyphOnly": true,
-    "text": "Previous"
-};
-
-var mobileNext = {
-    "glyph": "chevron-right",
-    "glyphOnly": true,
-    "text": "Next"
-};
-
-var actions = [
-    {
-        "glyph": "waffle",
-        "glyphOnly": true,
-        "text": "Menu"
-    },
-    {
-        "glyph": "search",
-        "glyphOnly": true,
-        "text": "Search"
-    },
-    {
-        "glyph": "shop-brand",
-        "glyphOnly": true,
-        "href": "#",
-        "text": "Bag"
-    }
-];
-
-// ==================================================================
-// Configuring other components
 
 // ==================================================================
 // Render app
+// Anything below is an introduction to consuming fluent web components.
 
 ReactDOM.render(
-    <div>
-        <Header
-            globalNavigation={ globalNavigation }
-            mobileToggle={ mobileToggle }
-            mobilePrevious={ mobilePrevious }
-            mobileNext={ mobileNext }
-            actions={ actions } 
-        />
-    </div>
+    <Page>
+        <Grid 
+            tag='section'
+            columnCount={12}
+            gutter={6}
+            verticalAlign={'center'}
+            horizontalAlign={'center'}
+            >
+            <Column
+                row={1}
+                span={[12,12,6]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                    level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[2,2,1]}
+                span={[12,12,6]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                   level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[3,3,2]}
+                span={[12,12,4]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                   level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[4,4,2]}
+                span={[12,12,4]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                   level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+            <Column
+                row={[5,5,2]}
+                span={[12,12,4]}
+                >
+                <Image
+                    vp5='http://placehold.it/740x417'
+                    vp4='http://placehold.it/582x328'
+                    vp3='http://placehold.it/494x278'
+                    vp2='http://placehold.it/720x405'
+                    vp1='http://placehold.it/491x276'
+                    alt="Placeholder"
+                    className='x-edge-hard'
+                />
+                <Heading
+                    tag="h3"
+                   level={5}
+                    text="Placeholder"
+                />
+                <Paragraph
+                    level={1}
+                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
+                />
+                <CallToAction
+                    weight="light"
+                    href="#"
+                    text="Shop now"
+                />
+            </Column>
+        </Grid>       
+    </Page>
     ,
     document.getElementById('root')
 );
