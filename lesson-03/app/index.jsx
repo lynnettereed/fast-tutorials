d1c3137e524459b5e93d55356952ba7aad7d4b0a
Jeff Smith <jes@microsoft.com>
2018-03-28 14:09:54 -0700

    Clean up Markup

--- lesson-03/app/index.jsx
+++ lesson-03/app/index.jsx
@@ -18,20 +18,20 @@ import logoMsGray from './assets/images/logos/microsoft-gray.png';
 
 // ==================================================================
 // Render app
+// Anything below introduces using layout elements from Fluent Web
 
 ReactDOM.render(
     <Page>
         <Grid 
-            tag='section1'
+            tag='section'
             columnCount={12}
             gutter={6}
             verticalAlign={'center'}
             horizontalAlign={'center'}
             >
-            {/* Row 1 */}
             <Column
                 row={1}
-                span={[12,12,6,6]}
+                span={[12,12,6]}
                 >
                 <Image
                     vp5='http://placehold.it/740x417'
@@ -58,8 +58,8 @@ ReactDOM.render(
                 />
             </Column>
             <Column
-                row={[2,2,1,1]}
-                span={[12,12,6,6]}
+                row={[2,2,1]}
+                span={[12,12,6]}
                 >
                 <Image
                     vp5='http://placehold.it/740x417'
@@ -86,8 +86,8 @@ ReactDOM.render(
                 />
             </Column>
             <Column
-                row={[3,3,2,2]}
-                span={[12,12,4,4]}
+                row={[3,3,2]}
+                span={[12,12,4]}
                 >
                 <Image
                     vp5='http://placehold.it/740x417'
@@ -114,8 +114,8 @@ ReactDOM.render(
                 />
             </Column>
             <Column
-                row={[4,4,2,2]}
-                span={[12,12,4,4]}
+                row={[4,4,2]}
+                span={[12,12,4]}
                 >
                 <Image
                     vp5='http://placehold.it/740x417'
@@ -142,8 +142,8 @@ ReactDOM.render(
                 />
             </Column>
             <Column
-                row={[5,5,2,2]}
-                span={[12,12,4,4]}
+                row={[5,5,2]}
+                span={[12,12,4]}
                 >
                 <Image
                     vp5='http://placehold.it/740x417'
@@ -157,118 +157,6 @@ ReactDOM.render(
                 <Heading
                     tag="h3"
                    level={5}
-                    text="Placeholder"
-                />
-                <Paragraph
-                    level={1}
-                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
-                />
-                <CallToAction
-                    weight="light"
-                    href="#"
-                    text="Shop now"
-                />
-            </Column>
-            <Column
-                row={[6,6,3,3]}
-                span={[12,12,6,6]}
-                >
-                <Image
-                    vp5='http://placehold.it/740x417'
-                    vp4='http://placehold.it/582x328'
-                    vp3='http://placehold.it/494x278'
-                    vp2='http://placehold.it/720x405'
-                    vp1='http://placehold.it/491x276'
-                    alt="Placeholder"
-                    className='x-edge-hard'
-                />
-                <Heading
-                    tag="h3"
-                   level={5}
-                    text="Placeholder"
-                />
-                <Paragraph
-                    level={1}
-                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
-                />
-                <CallToAction
-                    weight="light"
-                    href="#"
-                    text="Shop now"
-                />
-            </Column>
-            <Column
-                row={[7,7,3,3]}
-                span={[12,12,6,6]}
-                >
-                <Image
-                    vp5='http://placehold.it/740x417'
-                    vp4='http://placehold.it/582x328'
-                    vp3='http://placehold.it/494x278'
-                    vp2='http://placehold.it/720x405'
-                    vp1='http://placehold.it/491x276'
-                    alt="Placeholder"
-                    className='x-edge-hard'
-                />
-                <Heading
-                    tag="h3"
-                   level={5}
-                    text="Placeholder"
-                />
-                <Paragraph
-                    level={1}
-                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
-                />
-                <CallToAction
-                    weight="light"
-                    href="#"
-                    text="Shop now"
-                />
-            </Column>
-            <Column
-                row={[8,8,4,4]}
-                span={[12,12,6,6]}
-                >
-                <Image
-                    vp5='http://placehold.it/740x417'
-                    vp4='http://placehold.it/582x328'
-                    vp3='http://placehold.it/494x278'
-                    vp2='http://placehold.it/720x405'
-                    vp1='http://placehold.it/491x276'
-                    alt="Placeholder"
-                    className='x-edge-hard'
-                />
-                <Heading
-                    tag="h3"
-                    level={5}
-                    text="Placeholder"
-                />
-                <Paragraph
-                    level={1}
-                    text="Microsoft's mission is to empower every person and every organization on the planet to achieve more."
-                />
-                <CallToAction
-                    weight="light"
-                    href="#"
-                    text="Shop now"
-                />
-            </Column>
-            <Column
-                row={[9,9,4,4]}
-                span={[12,12,6,6]}
-                >
-                <Image
-                    vp5='http://placehold.it/740x417'
-                    vp4='http://placehold.it/582x328'
-                    vp3='http://placehold.it/494x278'
-                    vp2='http://placehold.it/720x405'
-                    vp1='http://placehold.it/491x276'
-                    alt="Placeholder"
-                    className='x-edge-hard'
-                />
-                <Heading
-                    tag="h3"
-                    level={5}
                     text="Placeholder"
                 />
                 <Paragraph
